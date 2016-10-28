f7c1d6adcd320fee6cbe643d0909e1df32e87494
patriciaa <patriciatorvalds@gmail.com>
2016-10-27 20:32:30 -0400

    added own phrases and keys

--- facebook_bot.js
+++ facebook_bot.js
@@ -1,5 +1,5 @@
-var page_token = ("insert your page_token from facebook here");
-var verify_token = ("insert your verify_token you set in fb developer console here");
+var page_token = ("EAAQfXvH7ppMBAOc02A4wppsD99q9pI9b2O1CTR3h0JRZA3EzY7Fz2mpV9LW6CGdwB4fSqp1c5N2wCTVIom2povjAFvgruAfljrrSdveShQahDvhU2XbpKd6tFIp7BODnY2myng1ZCOrK2PVDNhpMFX64CdZAzuG10RJzRcfQQZDZD");
+var verify_token = ("patpat");
 var bodyParser = require('body-parser');
 var express = require('express');
 var app = express();
@@ -17,12 +17,16 @@ var bot = controller.spawn({
 
 });
 
-controller.hears(['hello', 'hi'], 'message_received', function(bot, message) {
-    bot.reply(message, 'Hello.');
+controller.hears(['hello', 'hey', 'hi', 'merp', 'wat good'], 'message_received', function(bot, message) {
+    bot.reply(message, 'mirecourt');
 });
 
 // If you wanted your bot to respond to additional "hears" such as Go Heels 
 // you could do that below using the same syntax from above.
+
+controller.hears(['butts'], 'message_received', function(bot, message) {
+    bot.reply(message, 'butt butt');
+});
 
 
 //added to stop the debug tick remarks in console
