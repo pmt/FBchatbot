794272f22a585d8b836c9d07cb262a94bdb4d00c
Patricia Torvalds <patriciaa@github.com>
2016-11-12 20:51:35 -0500

    Starting point for JS

--- facebook_bot.js
+++ facebook_bot.js
@@ -1,5 +1,5 @@
-var page_token = ("EAAQfXvH7ppMBAOc02A4wppsD99q9pI9b2O1CTR3h0JRZA3EzY7Fz2mpV9LW6CGdwB4fSqp1c5N2wCTVIom2povjAFvgruAfljrrSdveShQahDvhU2XbpKd6tFIp7BODnY2myng1ZCOrK2PVDNhpMFX64CdZAzuG10RJzRcfQQZDZD");
-var verify_token = ("patpat");
+var page_token = ("EAAIJdgZBdh0wBAHdxOmbqppAA0DI7qNAp0BBbfPOz7mzQABkGyZBMle2ZBPgaaSx76lCsF0iE5qF72Iv5N3TVK44s3wVHTNhcOiKQlOaoZBp9P9mpmX1EnGcfzspQ5TZC5ZBZCPQ81ZBnzbQP9PoFbiEiHg5i6Jgbc0PtLRcjgeKkgZDZD");
+var verify_token = ("kameron");
 var bodyParser = require('body-parser');
 var express = require('express');
 var app = express();
@@ -17,24 +17,21 @@ var bot = controller.spawn({
 
 });
 
-controller.hears(['hello', 'hey', 'hi', 'merp', 'wat good'], 'message_received', function(bot, message) {
-    bot.reply(message, 'mirecourt');
+controller.hears(['hello', 'hi'], 'message_received', function(bot, message) {
+    bot.reply(message, 'Hello.');
 });
 
 // If you wanted your bot to respond to additional "hears" such as Go Heels 
 // you could do that below using the same syntax from above.
 
-controller.hears(['butts'], 'message_received', function(bot, message) {
-    bot.reply(message, 'butt butt');
-});
-
 
 //added to stop the debug tick remarks in console
 controller.on('tick', function(bot, event) {});
 
-controller.setupWebserver(process.env.port || 5000, function(err, webserver) {
+controller.setupWebserver(process.env.port || 7000, function(err, webserver) {
     controller.createWebhookEndpoints(webserver, bot, function() {
         console.log('ONLINE!');
 
     });
 });
+0
